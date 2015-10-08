f28316528126d292db9ede27b32121f4b61e250b
AdamGeipel <geipel.adam@uwlax.edu>
2015-10-08 00:01:09 -0500

    Update eventClass.js

--- eventClass.js
+++ eventClass.js
@@ -1,28 +1,16 @@
-var Event  = {
-	var title;
-	var location
-	var startDate;
-	var endDate;
-	var startTime;
-	var endTime;
-	var minVol;
-	var maxVol;
-	var numStudents;
-	var students;
-	var description;
-	
-	function printEvent(title, location, startDate, endDate, startTime, endTime, minVol, maxVol, numStudents, description) {
-		this.title = title;
-		this.location = location;
-		this.startDate = startDate;
-		this.endDate = endDate;
-		this.endTime = endTime;
-		this.minVol = minVol;
-		this.maxVol = maxVol;
-		this.numStudents = numStudents;
-		this.students = students;
-		this.description = description;
+var Event  = function(title, location, startDate, endDate, startTime, endTime, minVol, maxVol, numStudents,students, description) {
+	this.title = title;
+	this.location = location;
+	this.startDate = startDate;
+	this.endDate = endDate;
+	this.endTime = endTime;
+	this.minVol = minVol;
+	this.maxVol = maxVol;
+	this.numStudents = numStudents;
+	this.students = students;
+	this.description = description;
+}	
 
-	//print string of information
-	title	
-}
+person.prototype.print = function() {
+	
+}	
