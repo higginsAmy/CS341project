80ffe18610f0e866993f5182b7ff27a3e2011bc6
Administrator <higgins.amy@uwlax.edu>
2015-10-07 19:10:08 -0500

    modified authentication

--- login2.js
+++ login2.js
@@ -1,51 +1,104 @@
 /**
  * 
  */
+var db;
 
-$(document).ready(function()
-		{
+$(document).ready(function(){
+	initDB();
 	$("#auth").click(function(e){
 		e.preventDefault();
 		e.stopPropagation();
 		loginRecord();
 	});
-		});
+});
 
-
-//Define database options
-var dbOptions = {
-		fileName: "holmenRobotics.db",
-		version: "1.0",
-		displayName: "Holmen Robotics DataBase",
-		maxSize: 1024
-};
-//Now that we have our database properties defined, let's
-//open our database, getting a reference to the
-//generated connection.
-
-//NOTE: This might throw errors, but we're not going to
-//worry about that for this example.
-var db = openDatabase(
-		dbOptions.fileName,
-		dbOptions.version,
-		dbOptions.displayName,
-		dbOptions.maxSize
-);
+function initDB(){
+	try {
+		if (!window.openDatabase) {
+			alert('not supported');
+		} else {
+			//Define database options
+			var dbOptions = {
+					fileName: "holmenRobotics",
+					version: "1.0",
+					displayName: "Holmen Robotics DataBase",
+					maxSize: 1024
+			};
+			db = openDatabase(
+					dbOptions.fileName,
+					dbOptions.version,
+					dbOptions.displayName,
+					dbOptions.maxSize
+			);
+		}
+	} catch(e) {
+		// Error handling 
+		alert("Error opening the database");
+	}
+}
 
 function loginRecord(){
-
+	console.log(db);
 	db.transaction(function(tx){
 		var user=$('#username').val();
 		var pwd=$('#password').val();
-		var x='SELECT * FROM Users WHERE username="'+user+'" ';
-		console.log(x);
-		var y='SELECT * FROM Users WHERE username="'+user+'" AND password="'+pwd+'" '; //to get the username and password from the login table
-		console.log(y);
+		var x='SELECT username FROM Users WHERE username="'+user+'" ;';
+		var y='SELECT username, password, auth FROM Users WHERE username="'+user+'" AND password="'+pwd+'" ;';
+		tx.executeSql('CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY AUTOINCREMENT,' + 
+				' username TEXT UNIQUE, password TEXT , auth TEXT)');
+		tx.executeSql('SELECT username FROM Users WHERE username="admin"',
+				[], function(tx, result){
+				if (result.rows.length == 0){
+					tx.executeSql('INSERT INTO Users (username, password, auth) VALUES' +
+						'("admin", "password", "A")');
+				}
+		});
+		tx.executeSql('SELECT username FROM Users WHERE username="student"',
+				[], function(tx, result){
+				if (result.rows.length == 0){
+					tx.executeSql('INSERT INTO Users (username, password, auth) VALUES' +
+						'("student", "password", "S")');
+				}
+		});
+		tx.executeSql('SELECT username FROM Users WHERE username="volunteer"',
+				[], function(tx, result){
+				if (result.rows.length == 0){
+					tx.executeSql('INSERT INTO Users (username, password, auth) VALUES' +
+						'("volunteer", "password", "V")');
+				}
+		});
 		tx.executeSql(x,[],function(tx,result){
+			console.log(result.rows.length);
+			var h=result.rows.item(0);
 			if (result.rows.length > 0){
-				console.log(result.rows[0]);
-				alert("Username : " +  result.rows[0].username);
-				alert("Password: " + result.rows[0].password); 
+				alert("Username : " +  h['username']); 
+				tx.executeSql(y,[],function(tx,result){
+					console.log(result.rows.length);
+					if (result.rows.length > 0){
+						var h=result.rows.item(0);
+						if (h['auth'] == "A"){
+							var redirect = new String(window.location.href);
+							redirect = redirect.replace("Login.html", "Admin.html");
+							window.location.assign(redirect);
+						} 
+						else if (h['auth'] == "V"){
+							var redirect = new String(window.location.href);
+							redirect = redirect.replace("Login.html", "Volunteer.html");
+							window.location.assign(redirect);
+						}
+						else if (h['auth'] == "S"){
+							var redirect = new String(window.location.href);
+							redirect = redirect.replace("Login.html", "Student.html");
+							window.location.assign(redirect);
+						}
+						else {
+							alert("Your username currently has no authorization."+
+									" Please contact your administrator for help.");
+						}
+					}else{
+						alert("Invalid Password!");
+					}
+				});
 			}else{
 				alert("User not found!");
 			}
