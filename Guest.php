1c2a66ad92cfa6b2f3faa4ad4c47377b965c4d7b
higginsAmy <higgins.amy@uwlax.edu>
2015-11-12 01:16:10 -0600

    making calendars more uniform

--- Guest.php
+++ Guest.php
@@ -1,37 +1,27 @@
 <?php
-include('session.php');
-if (!isset($_SESSION['login_auth'])){
-	header("location: Guest.php");
-}
-switch($_SESSION['login_auth']){
-case "S":
-	header("location: Student.php"); // Redirecting To Student Page
-	break;
-case "V":
-	header("location: Volunteer.php"); // Redirecting To Volunteer Page
-	break;			
+include('login.php'); // Includes Login Script
+
+if(isset($_SESSION['login_user'])){
+header("location: profile.php");
 }
 ?>
 <!doctype html>
 <html>
   <head>
   	<meta charset="utf-8">
-    <title>You are logged as admin.</title>
- 	<!-- Styles --> 
+    <title>You are viewing this page as a guest</title>
+    <!-- Styles --> 
     <link rel="stylesheet" type="text/css" href="theme.css">
 	<link rel='stylesheet' href='fullcalendar/fullcalendar.css' />
 	<!-- Scripts -->
 	<script src='fullcalendar/lib/jquery.min.js'></script>
 	<script src='fullcalendar/lib/moment.min.js'></script>
 	<script src='fullcalendar/fullcalendar.js'></script>
-      
-      
-      
 	<script>
 		$(document).ready(function() {
     		// page is now ready, initialize the calendar...
     		$('#calendar').fullCalendar({
-				editable: true,
+        		editable: true,
         		weekmode: 'variable',
         		eventSources: [
 				// your event source
@@ -79,26 +69,26 @@ case "V":
 				]
     		})
 		});
+		
+		function redirect(){
+				var redirect = new String(window.location.href);
+				redirect = redirect.replace("Guest.html", "Login.html");
+				window.location.assign(redirect);
+		}
 	</script>
   </head>
   <body>
-  <div id = "title">
-    <a href="Admin.php">
-	<h2 id = "titleName">
-		<img id = "titleIcon" src = "calendar-icon.png"  alt="icon"> Holmen High School Robotics Club 
-    </h2>
-    </a>
-    <input id = "log" class="button"  type="button" onClick="location.href='logout.php'" value="Log out">
-    <input id = "changePassword" class="button"  type="button" onClick="location.href='changePassword.html'" value="Change password">
-  </div>      
-  <div id="label">
-    <input id = "ModifykHours" class="labelButton"  type="button" onClick="location.href='modifyStudentHours.html'" value="Modify student WorkHours ">
-    <input id = "addevent" class="labelButton"  type="button" onClick="location.href='forms/newEvent.php'" value="Add event">
-    <input id = "modifyuser" class="labelButton"  type="button" onClick="location.href='modifyUser.php'" value="Modify user">
-    <input id = "modifyItems" class="labelButton"  type="button" onClick="location.href='modifyItems.html'" value="Modify donation items">
-    <input id = "seeMessage" class="labelButton"  type="button" onClick="location.href='seeMessage.html'" value="See message">
-  </div>
-      
-  <div id='calendar' style="background:white; position:relative; top: 10px; width:75%; display:inline-block;"></div>
+    <div id="container"> 
+    	<div id="title">
+      		<h1 id="titleName">
+      		<img id = "icon"src="http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/calendar-icon.png">
+      		<b>Holmen High School Robotics Club</b>
+      		</h1>
+    	</div>
+		<div id='calendar' style="background:white; width:90%; display:inline-block;"></div>
+      	<button id="login" class="button" type="button" onclick="redirect()"><b>Log in</b></button>
+      	<button id="donate" class="button" type="button"><b>Donate</b></button>
+      	<button id="contact" class="button" type="button"><b>Contact</b></button>
+    </div>
   </body>
 </html>
